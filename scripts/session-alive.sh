e852daad80235b9d543a6bc1d3f5fab79cf93d32
vyapaari <devnetgo@gmail.com>
2025-10-20 23:03:28 +0530

    made changes to start and session script

--- scripts/session-alive.sh
+++ scripts/session-alive.sh
@@ -1,11 +1,5 @@
-# echo "=== KEEPING SESSION ACTIVE ==="
-# # Start a simple counting process
-# while true; do echo "$(date): Session active"; sleep 30; done &
-# KEEP_ALIVE_PID=$!
-# echo "Keep-alive process started: PID $KEEP_ALIVE_PID"
-
-jupyter server list
-
-cat ~/.config/code-server$ cat config.yaml
-
-ls -la 
+ echo "=== KEEPING SESSION ACTIVE ==="
+ # Start a simple counting process
+ while true; do echo "$(date): Session active"; sleep 30; done &
+ KEEP_ALIVE_PID=$!
+ echo "Keep-alive process started: PID $KEEP_ALIVE_PID"
