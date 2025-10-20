e13af1870378cf5f2a0741ff6b7c26218d63bd83
vyapaari <devnetgo@gmail.com>
2025-10-20 13:02:15 +0530

    keep sessoin active and md workspace moved to start

--- scripts/session-alive.sh
+++ scripts/session-alive.sh
@@ -1,5 +1,11 @@
-echo "=== KEEPING SESSION ACTIVE ==="
-# Start a simple counting process
-while true; do echo "$(date): Session active"; sleep 30; done &
-KEEP_ALIVE_PID=$!
-echo "Keep-alive process started: PID $KEEP_ALIVE_PID"
+# echo "=== KEEPING SESSION ACTIVE ==="
+# # Start a simple counting process
+# while true; do echo "$(date): Session active"; sleep 30; done &
+# KEEP_ALIVE_PID=$!
+# echo "Keep-alive process started: PID $KEEP_ALIVE_PID"
+
+jupyter server list
+
+cat ~/.config/code-server$ cat config.yaml
+
+ls -la 
